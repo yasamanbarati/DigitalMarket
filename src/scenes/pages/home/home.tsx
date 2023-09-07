12e4e17bc823c4449a5f9482d3d4dcbd3f155c03
yasaman <yasamanbarati81@gmail.com>
2023-09-07 13:14:50 +0330

    Improvment code structrue

--- src/scenes/pages/home/home.tsx
+++ src/scenes/pages/home/home.tsx
@@ -5,6 +5,8 @@ import { initializeAppData } from 'services/server/service'
 import { Footer } from 'scenes/footer/footer'
 import { TokenIntroduction } from 'scenes/body/homePage/token_introduction/token_introduction'
 import { TokenApplications } from 'scenes/body/homePage/token_applications/token_applications'
+import { IntroductionInvestment } from 'scenes/body/homePage/introduction_investment/introduction_investment'
+import { IntroducingApplication } from 'scenes/body/homePage/introducing_application/introducing_application'
 
 export const Home = () => {
 
@@ -15,6 +17,8 @@ export const Home = () => {
       <CoverSlider />
       <TokenIntroduction />
       <TokenApplications />
+      <IntroductionInvestment />
+      <IntroducingApplication />
       <Footer />
     </Fragment>
   )
--- src/scenes/slider/main/main_image/main_image.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export const main_image = () => {
-  return (
-    <div>main_image</div>
-  )
-}
